2a0a5ce0b976dce6e8eb7edc6c1afcb87db4b117
anaheey <130571829+anaheey@users.noreply.github.com>
2023-04-13 17:40:52 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -4,5 +4,3 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-let num:number;
-num=parseInt(prompt("numero"));
